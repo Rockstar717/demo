e0a8cab332ebbf0518b1c6df84704d4e9c504abe
Rockstar717 <warke.chetan710@gmail.com>
2023-03-06 14:39:24 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,47 @@
+package practice1;
+import java.util.Scanner;
+
+public class Addition {
+public static void main(String[] args) {
+
+
+// TODO Auto-generated method stub
+		/*int a= 2;
+		int b= 3;
+		int c=5;
+		int sum=(a+b+c);
+		System.out.println(sum);
+	}
+
+}*/
+		
+		//WAP cgpa number
+		
+		/*float subject1= 25;
+		float subject2=30;
+		float subject3=25;
+		float cgpa =(subject1+subject2+subject3)/30;
+		System.out.println(cgpa);
+	}}*/
+		
+		
+		
+		//use scanner class
+		
+		
+		/*Scanner sc = new Scanner(System.in);
+		System.out.println("what is your name");
+		String name = sc.nextLine();
+		System.out.println("hello "+name+" have good day");*/
+		
+
+            // WAP kilometer to mile
+	double km ,miles;
+	   Scanner sc = new Scanner (System.in);
+	   System.out.println("Enter the kilometer");
+	   km = sc.nextInt();
+	   
+	   miles = (km * 0.621371);
+	   System.out.println(+miles);
+   }
+}
